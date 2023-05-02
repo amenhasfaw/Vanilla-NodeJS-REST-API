a5e3540be6d75800fa16d9236e2c5dcce7d83965
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-05-02 17:31:42 +0530

    Basic HTTP Server

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const server = http.createServer((req,res) => {})
 
 const PORT = process.env.PORT || 5000
 
-server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
