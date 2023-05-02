96eff76e9eb5602f94717ec2a05cb0934f6a0244
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-05-02 18:09:20 +0530

    Created a simple API route

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require('http')
+const products = require('./Data/products.json')
 
-const server = http.createServer((req,res) => {})
+const server = http.createServer((req,res) => {
+    if(req.url === '/api/products' && req.method === 'GET'){
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.end(JSON.stringify(products))
+    }else{
+        res.writeHead(404,{'Content-Type':'application/json'})
+        res.end(JSON.stringify({message: 'Route not found'}))
+    }
+
+    
+})
 
 const PORT = process.env.PORT || 5000
 
-server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
+server.listen(PORT)
