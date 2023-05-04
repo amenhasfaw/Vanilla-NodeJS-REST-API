f8a99430f33742c9fced06444e30245e827da4f0
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-05-04 17:19:43 +0530

    POST product route done

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,7 @@
 const products = require('../Data/products.json')
+const { v4: uuidv4 } = require('uuid')
+
+const {writeDataToFile} =  require('../utils')
 
 function findAll(){
     return new Promise((resolve, reject) => {
@@ -13,8 +16,20 @@ function findById(id){
     })
 }
 
+function create(product) {
+    return new Promise((resolve, reject) => {
+        const newProduct = {id: uuidv4(), ...product}
+        products.push(newProduct)
+        if (process.env.NODE_ENV !== 'test') {
+            writeDataToFile('./data/products.json', products);
+        }
+        resolve(newProduct)
+    })
+}
+
 
 module.exports = {
     findAll,
-    findById
+    findById,
+    create
 }
